c32aa2136b12010addd6c2e1b115f6ee4a6ed192
Damir Vasiljevic <damir@sobot.biz>
2018-04-20 13:01:27 +0200

    added Application Insights support

--- server.js
+++ server.js
@@ -4,4 +4,6 @@
  * Module dependencies.
  */
 var app = require('./config/lib/app');
+const appInsights = require("applicationinsights")
+appInsights.setup("2fc0b169-09f3-4ee4-b7d2-05039a7fbe93")
 var server = app.start();
